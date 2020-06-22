09ff92e2ffca35e88007ada177d445cc7aaea9fb
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-22 13:06:46 +0200

    create matrix and generate number method

--- DAN_XXXVI_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXVI_Milan_Bratic/Zadatak_1/Program.cs
@@ -2,20 +2,52 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Zadatak_1
 {
     class Program
     {
+        private static readonly object locker = new object();
+        static int[] numbers = new int[10000];
+        static Random rnd = new Random();
+
+        static void Matrix()
+        {
+            lock (locker)
+            {
+                int[,] matrix = new int[100, 100];
+                Monitor.Wait(locker);
+                int index = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        matrix[i, j] = numbers[index];
+                        index++;
+                    }
+                }
+            }
+        }
+
+        static void GenerateNumber()
+        {
+            lock (locker)
+            {
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = rnd.Next(10, 100);
+                }
+                Monitor.Pulse(locker);
+            }
+        }
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Thread thread1 = new Thread(Matrix);
+            Thread thread2 = new Thread(GenerateNumber);
+            thread1.Start();
+            thread2.Start();
         }
     }
 }
