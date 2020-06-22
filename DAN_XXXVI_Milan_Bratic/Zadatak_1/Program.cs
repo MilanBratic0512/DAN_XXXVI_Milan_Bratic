f30fbeb318f225a05bc69f0b9d473adf2efa2a0c
MilanBratic0512 <milan.bratic2@gmail.com>
2020-06-22 13:40:57 +0200

    create method for generated odd numbers and read from the file

--- DAN_XXXVI_Milan_Bratic/Zadatak_1/Program.cs
+++ DAN_XXXVI_Milan_Bratic/Zadatak_1/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,15 +10,19 @@ namespace Zadatak_1
 {
     class Program
     {
+        
+        static string path = "../../MatrixFile.txt";
         private static readonly object locker = new object();
         static int[] numbers = new int[10000];
+        static int[,] matrix;
+        static List<int> oddNumbers = new List<int>();
         static Random rnd = new Random();
 
         static void Matrix()
         {
             lock (locker)
             {
-                int[,] matrix = new int[100, 100];
+                matrix = new int[100, 100];
                 Monitor.Wait(locker);
                 int index = 0;
                 for (int i = 0; i < 100; i++)
@@ -42,12 +47,48 @@ namespace Zadatak_1
                 Monitor.Pulse(locker);
             }
         }
+
+        static void OddMatrixNumber()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    if (matrix[i, j] % 2 != 0)
+                    {
+                        oddNumbers.Add(matrix[i, j]);
+                    }
+                }
+            }
+            using (TextWriter tw = new StreamWriter(path))
+            {
+                for (int i = 0; i < oddNumbers.Count; i++)
+                {
+                    tw.WriteLine(oddNumbers[i]);
+                }
+            }
+        }
+        static void ReadNumbersFromTheFile()
+        {
+            using (TextReader tr = new StreamReader(path))
+            {
+                Console.WriteLine(tr.ReadToEnd());
+            }
+        }
         static void Main(string[] args)
         {
             Thread thread1 = new Thread(Matrix);
             Thread thread2 = new Thread(GenerateNumber);
             thread1.Start();
             thread2.Start();
+            thread1.Join();
+            thread2.Join();
+            Thread thread3 = new Thread(OddMatrixNumber);
+            thread3.Start();
+            thread3.Join();
+            Thread thread4 = new Thread(ReadNumbersFromTheFile);
+            thread4.Start();
+            Console.ReadLine();
         }
     }
 }
